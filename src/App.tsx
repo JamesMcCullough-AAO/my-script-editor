f68159989bd1bc3974db58ce7bee2bff9063c819
James McCullough <james.mccullough@aaoglobal.com>
2023-09-02 13:00:46 +0100

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Textarea, VStack } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack spacing={4} p={5}>
+      <Box flex='1' width='100%' backgroundColor='tomato'>
+        <Textarea
+          placeholder="Type your script here..."
+          size="md"
+          height="1900px"
+          width="100%"
+          overflowY="auto"
+        />
+      </Box>
+    </VStack>
   );
 }
 
