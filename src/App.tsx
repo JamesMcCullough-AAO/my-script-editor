88163ee0bfec00c9efbe9a447302f671252fadd9
James McCullough <james.mccullough@aaoglobal.com>
2023-09-07 07:42:02 +0100

    remove label

--- src/App.tsx
+++ src/App.tsx
@@ -388,7 +388,6 @@ function App() {
         zIndex="100"
         padding="1em"
       >
-        // Green when above target, red when above max
         <Text
           fontWeight={600}
           fontSize="18px"
